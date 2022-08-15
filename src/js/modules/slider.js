b085fcfea6043168cba567d7aad1a09cf6075a1c
Stepanov Sergey <sergey11200011@mail.ru>
2022-08-15 19:31:24 +0500

    added slider and playVideo

---
+++ src/js/modules/slider.js
@@ -0,0 +1,65 @@
+export default class Slider {
+  constructor(page, btns) {
+    // Свойства
+    this.page = document.querySelector(page);
+    this.slides = this.page.children;
+    this.btns = document.querySelectorAll(btns);
+    this.slideIndex = 1;
+  }
+
+  // Методы
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+    try {
+      this.hanson.style.opacity = '0';
+
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('slideInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('slideInUp');
+      }
+    } catch (e) { }
+
+    this.slides.forEach(slide => {
+      slide.style.display = 'none';
+      slide.classList.add('animated', 'fadeIn');
+    });
+
+    this.slides[this.slideIndex - 1].style.display = 'block';
+  }
+
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) { }
+
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        this.plusSlides(1);
+      });
+      btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
+      }); // Перемещаемся по иерархии сначала выше к родителю, потом к предыдущему соседу родителя и находим элемент, на который будет клик
+    });
+
+    this.showSlides(this.slideIndex);
+  }
+
+}
